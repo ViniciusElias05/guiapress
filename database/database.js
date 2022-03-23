62ff294e33de86dd271321d902bfac537aeba1f6
ViniciusElias05 <vini-mq@hotmail.com>
2022-03-22 22:55:34 -0300

    atualizacao

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const sequelize = require("sequelize")
 
-const connection = new sequelize('guiapress', 'root', 'muitocurta801',{
+const connection = new sequelize('guiapress', 'root', '12345',{
     host: 'localhost',
     dialect: 'mysql',
     timezone: "-03:00"
